57db00419e5091a32789c852badf44c71120f69b
richardsonadjei <adjei.kwadjo@gmail.com>
2023-11-10 12:33:59 +0000

    made changes to the vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +13,10 @@ export default defineConfig({
   },
 
   plugins: [react()],
-})
+
+  build: {
+    rollupOptions: {
+      external: ['@fortawesome/fontawesome-svg-core'],
+    },
+  },
+});
