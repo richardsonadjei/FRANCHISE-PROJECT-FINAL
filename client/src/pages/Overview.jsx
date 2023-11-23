cf09c04af556aba8946c45de3e94e9d036899077
richardsonadjei <adjei.kwadjo@gmail.com>
2023-11-23 22:33:27 +0000

    Made changes to the overview page

--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -1,33 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import backgroundImage from '../../public/00071492.jpg'; // Replace with the actual path to your image
+import { useSelector } from 'react-redux';
 
-const OverviewPage = () => {
+const Overview = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-purple-500 to-indigo-600 text-white p-4 mt-28 md:p-8">
-      <h1 className="text-2xl md:text-4xl font-bold mb-4 md:mb-8 text-center">
-        <span className="text-pink-500">Pador</span> 
-        <span className="text-blue-400">Farms &nbsp;</span> 
-        <span className="text-orange-500">Warehouse</span>
-      </h1>
-      
-      <div className="flex flex-col gap-4">
-        <div className="p-4 md:p-8 bg-white rounded-lg shadow-lg text-center transform transition duration-300 hover:shadow-xl hover:scale-105">
-          <h2 className="text-xl md:text-2xl font-semibold text-gray-800 mb-2 md:mb-4">View Gallery Of Activities</h2>
-          <p className="text-gray-600 mb-4 md:mb-6">Get to know what has been done within the period in pictures.</p>
-          <a href="/buy" className="bg-indigo-600 text-white py-2 px-4 md:py-2 md:px-6 rounded-full hover:bg-indigo-700 transition duration-300 inline-block">Explore</a>
-        </div>
+    <div className="relative h-screen">
+      {/* Responsive image covering the entire page */}
+      <img
+        src={backgroundImage}
+        alt="Background"
+        className="object-cover w-full h-full"
+      />
+
+      {/* Centered content */}
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white w-full">
         
-        <div className="p-4 md:p-8 bg-white rounded-lg shadow-lg text-center transform transition duration-300 hover:shadow-xl hover:scale-105">
-          <h2 className="text-xl md:text-2xl font-semibold text-gray-800 mb-2 md:mb-4">To-Do-List</h2>
-          <p className="text-gray-600 mb-4 md:mb-6">View pending list of activities for the year.</p>
-          <a href="/tasks" className="bg-indigo-600 text-white py-2 px-4 md:py-2 md:px-6 rounded-full hover:bg-indigo-700 transition duration-300 inline-block">Pending Activities</a>
+        
+        <div className="mt-8">
+          {/* Use Link to navigate to /dashboard */}
+          <Link to="/home">
+            <button className="btn btn-primary mx-auto"> {/* Added mx-auto for horizontal centering */}
+              <span>Get Started</span>
+            </button>
+          </Link>
         </div>
       </div>
-      
-      <footer className="mt-8 md:mt-20 text-gray-400 text-center py-2 md:py-4">
-        <p>&copy; {new Date().getFullYear()} Pador Farms Warehouse. All rights reserved. Built By..@Ajar.</p>
+
+      {/* Footer */}
+      <footer className="absolute bottom-0 w-full text-center text-white font-bold py-2">
+        <p>&copy; {new Date().getFullYear()} Pador Farms Warehouse. All rights reserved. </p>
+        <p>Built By <span style={{ fontWeight: 'bold', fontStyle: 'italic' }}>@Richardson</span>.</p>
       </footer>
     </div>
   );
 };
 
-export default OverviewPage;
+export default Overview;
